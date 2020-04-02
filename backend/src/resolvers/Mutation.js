841be7c827fd4b26e64f1e78fb43346abfab610b
Ciaran Grimshaw <ciaran.grimshaw@dontpanicprojects.com>
2020-04-02 11:57:14 +0100

    Implementation of Mutation and Query custom resolvers, updating prisma datamodel

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,15 @@
-const Mutations = {};
+// custom resolver for Mutations
+
+const Mutations = {
+    async createItem(parent, args, ctx, info) {
+        // TODO: check if logged in
+
+        const item = await ctx.db.mutation.createItem({
+            data: { ...args }
+        }, info);
+
+        return item;
+    }
+};
 
 module.exports = Mutations;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,9 @@
-const Query = {};
+// custom resolver for Querying
+
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+    items: forwardTo('db'),
+};
 
 module.exports = Query;
