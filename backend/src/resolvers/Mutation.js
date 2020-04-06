8d6768f0541bb95031d52a33c9580ad130734830
Ciaran Grimshaw <ciaran.grimshaw@dontpanicprojects.com>
2020-04-06 16:37:45 +0100

    Update item functionality

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -9,7 +9,26 @@ const Mutations = {
         }, info);
 
         return item;
-    }
+    },
+
+    updateItem(parent, args, ctx, info) {
+        // take a copy of the updates
+        const updates = { ... args };
+
+        // remove ID from the updates, we don't want to update that
+        delete updates.id;
+
+        // run update method
+        return ctx.db.mutation.updateItem(
+            {
+                data: updates,
+                where: {
+                    id: args.id,
+                }
+            },
+            info
+        );
+    },
 };
 
 module.exports = Mutations;
