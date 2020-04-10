cecf1692230299a252234da7835e8456fb585a4e
Ciaran Grimshaw <ciaran.grimshaw@dontpanicprojects.com>
2020-04-10 18:20:19 +0100

    Pagination controls

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -3,6 +3,7 @@ import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import styled from 'styled-components';
 import Item from './Item';
+import Pagination from './Pagination';
 
 const ALL_ITEMS_QUERY = gql`
     query ALL_ITEMS_QUERY {
@@ -31,20 +32,24 @@ const ItemsList = styled.div`
 
 class Items extends Component {
     render() {
-        return <div>
-            <p>Items</p>
-            <Query query={ALL_ITEMS_QUERY}>
-                {({ data, error, loading }) => {
-                    if (loading) return <p>Loading...</p>;
-                    if (error) return <p>Error: {error.message}</p>;
-                    return <ItemsList>
-                        {data.items.map(item => <Item item={item} key={item.id} />)}
-                    </ItemsList>;
-                }
-                
-                }
-            </Query>
-        </div>;
+        return (
+            <Center>
+                <Pagination page={this.props.page} />
+                <p>Items</p>
+                <Query query={ALL_ITEMS_QUERY}>
+                    {({ data, error, loading }) => {
+                        if (loading) return <p>Loading...</p>;
+                        if (error) return <p>Error: {error.message}</p>;
+                        return <ItemsList>
+                            {data.items.map(item => <Item item={item} key={item.id} />)}
+                        </ItemsList>;
+                    }
+                    
+                    }
+                </Query>
+                <Pagination page={this.props.page} />
+            </Center>
+        );
     }
 }
 
