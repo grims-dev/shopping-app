3a196c84d2a87af4bfa9b7c9c89fdd0c1b56afe4
Ciaran Grimshaw <ciaran.grimshaw@dontpanicprojects.com>
2020-04-25 13:20:17 +0100

    Add check in CartItem for if the item has been deleted

--- frontend/components/CartItem.js
+++ frontend/components/CartItem.js
@@ -19,7 +19,18 @@ const CartItemStyles = styled.li`
     }
 `;
 
-const CartItem = ({ cartItem }) => (
+
+
+const CartItem = ({ cartItem }) => {
+    // first check if the item exists
+    if (!cartItem.item) return (
+    <CartItemStyles>
+        <p>This item has been removed.</p>
+        <RemoveFromCart id={cartItem.id} />
+    </CartItemStyles>
+    );
+    
+    return (
     <CartItemStyles>
         <img width="100" src={cartItem.item.image} alt={cartItem.item.title} />
         <div className="cart-item-details">
@@ -34,7 +45,7 @@ const CartItem = ({ cartItem }) => (
         </div>
         <RemoveFromCart id={cartItem.id} />
     </CartItemStyles>
-);
+)};
 
 CartItem.propTypes = {
     cartItem: PropTypes.object.isRequired,
