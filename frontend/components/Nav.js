9b14a56faf1b27c8ced4cb0a881d8d5cc3282be3
Ciaran Grimshaw <ciaran.grimshaw@dontpanicprojects.com>
2020-04-25 13:12:58 +0100

    Adding CartCount component with CSS animations on update

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -4,6 +4,7 @@ import { TOGGLE_CART_MUTATION } from './Cart';
 import NavStyles from './styles/NavStyles';
 import User from './User';
 import Signout from './Signout';
+import CartCount from './CartCount';
 
 const Nav = () => (
     <User>
@@ -27,7 +28,10 @@ const Nav = () => (
                     <Signout />
                     <Mutation mutation={TOGGLE_CART_MUTATION}>
                         {(toggleCart) => (
-                            <button onClick={toggleCart}>My Cart</button>
+                            <button onClick={toggleCart}>
+                                My Cart
+                                <CartCount count={me.cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0)} />
+                            </button>
                         )}
                     </Mutation>
                     
