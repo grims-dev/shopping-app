e6826d843594613a185d711070692c179a718f7a
Ciaran Grimshaw <ciaran.grimshaw@dontpanicprojects.com>
2020-04-28 13:38:17 +0100

    Initial stripe connection

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -4,6 +4,7 @@ import gql from 'graphql-tag';
 import { adopt } from 'react-adopt';
 import User from './User';
 import CartItem from './CartItem';
+import TakeMyMoney from './TakeMyMoney';
 import CartStyles from './styles/CartStyles';
 import Supreme from './styles/Supreme';
 import CloseButton from './styles/CloseButton';
@@ -47,7 +48,9 @@ const Cart = () => (
                     </ul>
                     <footer>
                         <p>{formatMoney(calcTotalPrice(me.cart))}</p>
-                        <SickButton>Checkout</SickButton>
+                        <TakeMyMoney>
+                            <SickButton>Checkout</SickButton>
+                        </TakeMyMoney>
                     </footer>
                 </CartStyles>
             )
